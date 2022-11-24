3cc774bf3ec8ad65ba43166201de74ce378241a6
Tony Eng <tony.eng@telus.com>
2022-11-24 14:46:38 -0800

    finished breedFetcherTest.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,24 @@
-let args = process.argv.slice(2);
 const request = require('request');
 
-let search = args[0];
 
-request(`https://api.thecatapi.com/v1/breeds/search/?q=${search}`, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log('requested breed not found');
-  }
-  // console.log(data);
-  try {
-    console.log(data[0].description);
-  } catch (error) {
-    console.log('error', error.message);
-  }
-});
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(null, 'requested breed not found');
+      return;
+    }
+    // console.log(data);
+    try {
+      callback(null, data[0].description);
+    } catch (error) {
+      callback(error, null);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
